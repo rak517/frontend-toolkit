ebd9844433180d123053508301bae7fd77475b58
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-11-06 17:12:43 +0900

    :wrench: chore: 공통 설정 최적화

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,38 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['@typescript-eslint'],
   root: true,
   env: {
     browser: true,
     es2020: true,
+    node: true,
   },
   rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
+  ignorePatterns: [
+    'dist',
+    '.next',
+    'node_modules',
+    '*.config.js',
+    '*.config.ts',
+    '*.config.mjs',
+  ],
 };
