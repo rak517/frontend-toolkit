5e8178ebd1f3052a23f1ea044930a856feb66a7c
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-11-06 17:09:09 +0900

    :recycle: refactor: _meta.json deprecated 오류로 수정

---
+++ apps/docs/pages/_meta.ts
@@ -0,0 +1,20 @@
+export default {
+  index: {
+    title: '소개',
+    theme: {
+      layout: 'full',
+    },
+  },
+  'getting-started': {
+    title: '시작하기',
+  },
+  hooks: {
+    title: 'Hooks',
+  },
+  components: {
+    title: 'Components',
+  },
+  utils: {
+    title: 'Utils',
+  },
+} as const;
