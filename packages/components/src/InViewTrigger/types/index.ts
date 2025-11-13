88624296f5e84845d1f83abf5ad9f3387baa0afa
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-11-13 18:04:20 +0900

    :sparkles: feat: InViewTrigger props 타입 선언

---
+++ packages/components/src/InViewTrigger/types/index.ts
@@ -0,0 +1,15 @@
+import type { CSSProperties, ReactNode } from 'react';
+
+export interface InViewTriggerProps {
+  onInView: (entry?: IntersectionObserverEntry) => void;
+  onOutView: (entry?: IntersectionObserverEntry) => void;
+  threshold?: number | number[];
+  root?: Element | null;
+  rootMargin?: string;
+  triggerOnce?: boolean;
+  disabled?: boolean;
+  children: ReactNode;
+  className?: string;
+  style?: CSSProperties;
+  debounce?: number;
+}
