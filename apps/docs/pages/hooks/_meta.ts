7ddcbaa5ddf5712a7b55e2708688dafed6bb7292
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-11-22 17:04:01 +0900

    :memo: docs: useIsMounted 문서 추가

--- apps/docs/pages/hooks/_meta.ts
+++ apps/docs/pages/hooks/_meta.ts
@@ -1,6 +1,6 @@
 export default {
   'use-calendar': 'useCalendar',
-  // 'use-is-mounted': 'useIsMounted',
+  'use-is-mounted': 'useIsMounted',
   // 'use-debounce': 'useDebounce',
   // 'use-debounced-callback': 'useDebouncedCallback',
 } as const;
