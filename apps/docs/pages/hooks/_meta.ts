e8d9eff23865811970073c9fbebe5c39de1f87a6
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-11-22 17:07:58 +0900

    :memo: docs: useDebouncedCallback 문서 추가

--- apps/docs/pages/hooks/_meta.ts
+++ apps/docs/pages/hooks/_meta.ts
@@ -2,5 +2,5 @@ export default {
   'use-calendar': 'useCalendar',
   'use-is-mounted': 'useIsMounted',
   'use-debounce': 'useDebounce',
-  // 'use-debounced-callback': 'useDebouncedCallback',
+  'use-debounced-callback': 'useDebouncedCallback',
 } as const;
