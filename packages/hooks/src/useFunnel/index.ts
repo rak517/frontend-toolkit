aa760bcc9190f62d89f05ad32855f220c3e4d297
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-12-05 16:05:56 +0900

    :sparkles: feat: useFunnel export 추가

---
+++ packages/hooks/src/useFunnel/index.ts
@@ -0,0 +1,9 @@
+export { useFunnel } from './useFunnel';
+export { useFunnelStep } from './context';
+export type {
+  FunnelProps,
+  FunnelStepProps,
+  UseFunnelOptions,
+  UseFunnelReturn,
+  FunnelContextValue,
+} from './types';
