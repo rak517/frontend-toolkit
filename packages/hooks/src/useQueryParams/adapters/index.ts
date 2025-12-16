ffb96fa676dedb756571f5ec869016a083647057
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-12-16 15:50:43 +0900

    :recycle: refactor: 어댑터 index에서 export 정리

--- packages/hooks/src/useQueryParams/adapters/index.ts
+++ packages/hooks/src/useQueryParams/adapters/index.ts
@@ -1,5 +1,12 @@
 export { createBrowserAdapter } from './browser';
+export { useNextAppAdapter } from './next-app';
+export { useNextPagesAdapter } from './next-pages';
+export { useReactRouterAdapter } from './react-router';
+
 export type {
   QueryParamsAdapter,
   BrowserAdapterOptions,
 } from './types';
+export type { NextAppAdapterOptions } from './next-app';
+export type { NextPagesAdapterOptions } from './next-pages';
+export type { ReactRouterAdapterOptions } from './react-router';
