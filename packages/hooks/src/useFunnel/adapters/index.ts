fc28cfc671ea3fcc9e4024a16baa7f7c05793645
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-12-19 18:22:12 +0900

    :sparkles: feat: 프레임워크 어댑터 추가 index 수정

--- packages/hooks/src/useFunnel/adapters/index.ts
+++ packages/hooks/src/useFunnel/adapters/index.ts
@@ -1,7 +1,13 @@
 export { createMemoryAdapter } from './memory';
 export { createBrowserAdapter } from './browser';
+export { useNextAppAdapter } from './next-app';
+export { useNextPagesAdapter } from './next-pages';
+export { useReactRouterAdapter } from './react-router';
 export type {
   FunnelAdapter,
   MemoryAdapterOptions,
   BrowserAdapterOptions,
 } from './types';
+export type { NextAppAdapterOptions } from './next-app';
+export type { NextPagesAdapterOptions } from './next-pages';
+export type { ReactRouterAdapterOptions } from './react-router';
